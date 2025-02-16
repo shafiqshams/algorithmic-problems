51058cfef233e1cb994b3cf8a588245154debccc
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-02-16 02:39:50 +0100

    solve valid-anagram using hash map (freq counter)

--- valid-anagram.ts
+++ valid-anagram.ts
@@ -1,9 +1,25 @@
 function isAnagram(s: string, t: string): boolean {
 
+   const counterOfS: { [key: string]: number } = {}
+   const counterOfT: { [key: string]: number } = {}
+
    if (s.length !== t.length)
       return false
 
-   return (s.split('').sort().join('') === t.split('').sort().join(''))
+   for (let index = 0; index < s.length; index++) {
+      counterOfS[s[index]] = (counterOfS[s[index]] || 0) + 1
+      counterOfT[t[index]] = (counterOfT[t[index]] || 0) + 1
+   }
+
+   for (const key in counterOfS) {
+      if (counterOfS[key] !== counterOfT[key])
+         return false
+   }
+
+   return true
+   // return (s.split('').sort().join('') === t.split('').sort().join(''))
 };
+
+
 
 console.log(isAnagram('rat', 'tar'))
