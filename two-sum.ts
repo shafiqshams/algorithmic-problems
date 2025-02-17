016c161b97febf7520b3cbd37944ecc10aa67c9e
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-02-17 17:32:58 +0100

    solve two sum problem with brute force approach

---
+++ two-sum.ts
@@ -0,0 +1,26 @@
+
+
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+function twoSum(nums: number[], target: number): number[] {
+   const twoSum: number[] = []
+
+   if (nums.length <= 2)
+      return [0]
+
+   // Time complexity: O(n^2)
+   for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+         if (nums[i] + nums[j] === target) {
+            twoSum.push(i, j)
+         }
+      }
+   }
+   return twoSum;
+};
+
+const res = twoSum([7, 11, 15, 2], 9);
